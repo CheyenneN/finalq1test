dc00e2fff305c7d7563b7a5ac27b44b4892d518a
Cheyenne <Cheyenne@128.4.53.70>
2014-12-08 01:58:21 -0500

    testing

---
+++ src/codingtest/test.java
@@ -0,0 +1,19 @@
+package codingtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void test() {
+		pass("ownership of truck should go from personA to personB");
+	}
+
+	private void pass(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
